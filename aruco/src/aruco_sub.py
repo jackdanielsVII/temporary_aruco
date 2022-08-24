a09addafaed004913ad0d3251621bdb0fbc7794a
Kevin Yeong_Min Choi <94097743+jackdanielsVII@users.noreply.github.com>
2022-08-24 11:00:04 +0900

    Update aruco_sub.py

--- aruco/src/aruco_sub.py
+++ aruco/src/aruco_sub.py
@@ -1,10 +1,12 @@
 #!/home/kevin/.pyenv/versions/pyenv_py385/bin/python
 
 import rospy
-from std_msgs.msg import Bool, Pose
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Pose
 
 
 def main():
+    rospy.init_node("aruco_sub")
     rospy.Subscriber('aruco_xyzw', Pose, find_aruco_pose_callback)
     rospy.Subscriber('check_aruco', Bool, check_aruco_pose_callback)
     rospy.spin()
